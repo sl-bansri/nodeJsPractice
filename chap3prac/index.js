a3b88c214aaf71395d82cf2d64862f88521c9f20
sl-bansri <bansri.savannah@gmail.com>
2025-06-30 06:47:34 -0700

    uuid , event emitter , chap5 webserver half

--- chap3prac/index.js
+++ chap3prac/index.js
@@ -1,2 +1,15 @@
-const { format } = require("date-fns");
-console.log(new Date(), "yyyyMMdd\tHH:MM:SS");
+const LogEvents = require("./LogEvents");
+const EventsEmitter = require("events");
+
+class MyEmitter extends EventsEmitter {}
+
+// initiall object
+const myEmitter = new MyEmitter();
+
+// add listener for event log
+
+myEmitter.on("log", (msg) => LogEvents(msg));
+
+setTimeout(() => {
+  myEmitter.emit("log", "log event emitted");
+}, 2000);
