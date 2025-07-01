20880d9c17a3d6d54a7d4c7d1265e80805819e6e
sl-bansri <bansri.savannah@gmail.com>
2025-07-01 04:02:51 -0700

    create server using eventEmitter

--- chap5prac/server.js
+++ chap5prac/server.js
@@ -10,13 +10,35 @@ class Emitter extends EventsEmitter {}
 
 // initiall object
 const myEmitter = new Emitter();
+myEmitter.on("log", (msg, fileName) => LogEvents(msg, fileName));
 
-const PORT = process.env.PORT || 3500;
+const PORT = process.env.PORT || 5173;
+
+const serveFile = async (filePath, contentType, response) => {
+  try {
+    const rawData = await fsPromises.readFile(
+      filePath,
+      !contentType.includes("image") ? "utf8" : ""
+    );
+    const data =
+      contentType === "application/json" ? JSON.parse(rawData) : rawData;
+    response.writeHead(filePath.includes("404.html") ? 404 : 200, {
+      "Content-Type": contentType,
+    });
+    response.end(
+      contentType === "application/json" ? JSON.stringify(data) : data
+    );
+  } catch (err) {
+    console.log(err);
+    myEmitter.emit("log", `${err.name}:${err.message}`, "errorLog.txt");
+    response.statusCode = 500;
+    response.end();
+  }
+};
 
 const server = http.createServer((req, res) => {
-  console.log(`${req.url} url , ${req.method} method`);
-
-  let path;
+  console.log(`${req.url} url \n, ${req.method} method \n`);
+  myEmitter.emit("log", `${req.url}\t${req.method}`, "reqLog.txt");
 
   const extension = path.extname(req.url);
 
@@ -53,7 +75,32 @@ const server = http.createServer((req, res) => {
       : contentType === "text/html"
       ? path.join(__dirname, "views", req.url)
       : path.join(__dirname, req.url);
+
+  // makes .html extension not required in the browser
+  if (!extension && req.url.slice(-1) !== "/") filePath += "./html";
+
+  const fileExists = fs.existsSync(filePath);
+
+  if (fileExists) {
+    // serve the file
+    serveFile(filePath, contentType, res);
+  } else {
+    switch (path.parse(filePath).base) {
+      case "old-page.html":
+        res.writeHead(301, { Location: "/new-page.html" });
+        res.end();
+        break;
+      case "www-page.html":
+        res.writeHead(301, { Location: "/" });
+        res.end();
+        break;
+      default:
+        serveFile(path.join(__dirname, "views", "404.html"), "text/html", res);
+    }
+  }
 });
+
+server.listen(PORT, () => console.log(`server running on port ${PORT}`));
 
 //   if (req.url === "/" || req.url === "index.html") {
 //     res.statusCode = 200;
@@ -78,5 +125,3 @@ const server = http.createServer((req, res) => {
 //   }
 
 // OR
-
-server.listen(PORT, () => console.log(`server running on port ${PORT}`));
